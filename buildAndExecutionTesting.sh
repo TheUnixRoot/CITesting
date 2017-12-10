708127a5b283c34b60ca582d47c4dce934722b30
Juan Pedro Domínguez Berdún <juanpedrodb.96@gmail.com>
2017-12-10 12:23:06 +0100

    Build and execute process finished

--- buildAndExecutionTesting.sh
+++ buildAndExecutionTesting.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
-mode=debug
-git=https://github.com/TheUnixRoot/CITesting.git
-url=https://github.com/01org/tbb/archive/2018_U2.tar.gz
+mode=debug # or release
+git=https://github.com/TheUnixRoot/CITesting.git #
+url=https://github.com/01org/tbb/archive/2018_U2.tar.gz #
 git clone "$git"
-filename=$(basename "$url")
+filename=$(basename "$url") #
 wget "$url"
 tar xzf $filename
 mv "tbb-2018_U2" ./tbb-2018
 cd ./tbb-2018
 make
 cd build
-mv *_debug linux_debug
-mv *_release linux_release
-source "linux_$mode"
-cd ../..
-cmake --build ./CITesting/cmake-build-debug --target prPractica2
-prPractica2 > ../log.txt
+filename=$(ls | egrep _$mode) #
+echo $filename
+cd $filename
+chmod 777 tbbvars.sh
+sed -i -e "s/$filename/linux_$mode/g" tbbvars.sh
 cd ..
-pause
-rm -r $OLDPWD 
+mv $filename linux_$mode
+chmod 777 "linux_$mode/tbbvars.sh"
+source "linux_$mode/tbbvars.sh"
+cd ../../CITesting
+mkdir cmake-build-$mode
+cd cmake-build-$mode
+cmake ..
+make
+cd ../../
+./CITesting/cmake-build-$mode/prPractica2 > ../log.txt
+read -n1 -p "Press any key to clean workspace Ctrl-C otherwise"
+yes | rm -r ./* 
